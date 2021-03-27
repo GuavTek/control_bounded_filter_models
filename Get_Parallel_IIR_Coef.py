26ff0e361f6cc41b2f873a2efea9a509c6fb658d
GuavTek <36814224+GuavTek@users.noreply.github.com>
2021-03-27 20:53:12 +0100

    Upload Scripts

---
+++ Get_Parallel_IIR_Coef.py
@@ -0,0 +1,202 @@
+import csv
+import numpy as np
+import scipy.linalg as la
+
+WriteFiles = 1
+N = 3
+
+Af = []
+Ab = []
+Bf = []
+Bb = []
+W = []
+
+def WriteFile (name, data):
+	CSVfile = open('data/' + name + '.csv', 'w', newline='')
+	fileWriter = csv.writer(CSVfile, delimiter=';')
+	fileWriter.writerows(map(lambda x: [x], data))
+	CSVfile.close()
+
+def WriteHeader (length):
+	global f
+	f = open("data/Coefficients.h", 'w')
+	Write1D("Lf", Lf)
+	Write1D("Lb", Lb)
+	Write1D("Wf", Wf)
+	Write1D("Wb", Wb)
+	Write2D("Ff", Ff)
+	Write2D("Fb", Fb)
+
+	Lbw = np.zeros((N, length), complex)
+	# Pre-calculate constants
+	for i in range(0, N):
+		for j in range(0, length):
+			Lbw[i][j] = (Wb[i] * Lb[i]**j)
+	
+	f.write("const float Lbwr[%d][%d] = {\n" %(N,length))
+	for i in range(0, N):
+		f.write("\t{")
+		for j in range(0, length):
+			if (j > 0):
+				f.write(", ")
+			f.write(str(Lbw[i][j].real))
+		if (i < N-1):
+			f.write("},\n")
+		else:
+			f.write("}")
+	f.write("};\n\r")
+	f.write("const float Lbwi[%d][%d] = {\n" %(N,length))
+	for i in range(0, N):
+		f.write("\t{")
+		for j in range(0, length):
+			if (j > 0):
+				f.write(", ")
+			f.write(str(Lbw[i][j].imag))
+		if (i < N-1):
+			f.write("},\n")
+		else:
+			f.write("}")
+	f.write("};\n\r")
+
+	f.close()
+
+def Write1D (name, data):
+	f.write("const float " + name + "r[%d] = {" %N)
+	for i in range(0, N):
+		if (i > 0):
+			f.write(", ")
+		f.write(str(data[i].real))
+	f.write("};\n\r")
+	f.write("const float " + name + "i[%d] = {" %N)
+	for i in range(0, N):
+		if (i > 0):
+			f.write(", ")
+		f.write(str(data[i].imag))
+	f.write("};\n\r")
+
+def Write2D (name, data):
+	f.write("const float " + name + "r[%d][%d] = {\n" %(N,N))
+	for i in range(0, N):
+		f.write("\t{")
+		for j in range(0, N):
+			if (j > 0):
+				f.write(", ")
+			f.write(str(data[i][j].real))
+		if (i < N-1):
+			f.write("},\n")
+		else:
+			f.write("}")
+	f.write("};\n\r")
+	f.write("const float " + name + "i[%d][%d] = {\n" %(N,N))
+	for i in range(0, N):
+		f.write("\t{")
+		for j in range(0, N):
+			if (j > 0):
+				f.write(", ")
+			f.write(str(data[i][j].imag))
+		if (i < N-1):
+			f.write("},\n")
+		else:
+			f.write("}")
+	f.write("};\n\r")
+
+#### Load offline matrices ####
+with open('offline_matrices.csv', newline='') as csvfile:
+	test = csv.reader(csvfile, delimiter=';')
+	targetVar = "0"
+	for line in test:
+		if(line == []):
+			continue
+		try:
+			tempFloat = []
+			for num in line:
+				tempFloat.append(float(num))
+			#print(tempFloat)
+			if(targetVar == 'Af'):
+				Af.append(tempFloat)
+			elif(targetVar == 'Ab'):
+				Ab.append(tempFloat)
+			elif(targetVar == 'Bf'):
+				Bf.append(tempFloat)
+			elif(targetVar == 'Bb'):
+				Bb.append(tempFloat)
+			elif(targetVar == 'W'):
+				W.append(tempFloat)
+		except:
+			#Save which variable we are reading
+			targetVar = line[0]
+			#print("Target is: " + str(line[0]))
+	csvfile.close()
+
+sig = []
+#### Load Test-data ####
+with open('control_signals.csv', newline='') as csvfile:
+	test = csv.reader(csvfile, delimiter=';')
+	for line in test:
+		if(line == []):
+			continue
+		temp = []
+		for num in line:
+			x = num.split(',')
+			for y in x:
+				if (y == '') or (y == '0'):
+					continue
+				temp.append(int(y))
+		sig.append(temp)
+	csvfile.close()
+
+#Convert test-signals to hardware levels
+hardSig = []
+for line in sig:
+	temp = []
+	temp = [0 if item == -1 else item for item in line]
+	hardSig.append(temp)
+
+#Convert lists to arrays
+Af = np.array(Af)
+Ab = np.array(Ab)
+Bf = np.array(Bf)
+Bb = np.array(Bb)
+#Make W a 1D array
+W = np.array(W)
+W = W.reshape(3)
+
+#Get eigenvectors and eigenvalues
+Lf, Qf = la.eig(Af)
+Lb, Qb = la.eig(Ab)
+
+#Inverted eigenvectors
+Qif = np.linalg.inv(Qf)
+Qib = np.linalg.inv(Qb)
+
+#Vectorized functions
+Ff = np.dot(Qif, Bf)
+Fb = np.dot(Qib, Bb)
+
+Wf = np.zeros(3)
+Wb = np.zeros(3)
+
+#Final coefficients
+Wf = -np.dot(Qf.T, W)
+Wb = np.dot(Qb.T, W)
+
+#Convert to lists
+Lf = Lf.tolist()
+Lb = Lb.tolist()
+Ff = Ff.tolist()
+Fb = Fb.tolist()
+Wf = Wf.tolist()
+Wb = Wb.tolist()
+
+
+#Write CSV files
+if (WriteFiles):
+	WriteFile('Lf', Lf)
+	WriteFile('Lb', Lb)
+	WriteFile('Ff', Ff)
+	WriteFile('Fb', Fb)
+	WriteFile('Wf', Wf)
+	WriteFile('Wb', Wb)
+	WriteHeader(128)
+	WriteFile('hardware_signals', hardSig)
+	WriteFile('clean_signals', sig)
---
+++ Test_Architecture.py
@@ -0,0 +1,486 @@
+import csv
+import numpy as np
+import matplotlib as mpl
+import enum
+from matplotlib import pyplot as plt
+from scipy import signal
+
+typeLabel = '32bit'
+floatType = np.float32
+complexType = np.complex64
+
+signalLength = 16384
+N = 3
+f_clk = 504e6
+fs = 5e6
+
+Af = []
+Ab = []
+Bf = []
+Bb = []
+W = []
+S = []
+
+def Complex32(var: complex) -> complex:
+	var.real = np.float16(var.real)
+	var.imag = np.float16(var.imag)
+	return var
+
+def ReadFile(fileName):
+	csvfile = open(fileName, newline='')
+	r = csv.reader(csvfile, delimiter=',')
+	temp = []
+	for line in r:
+		tempList = []
+		for num in line:
+			num = num.replace("[", "")
+			num = num.replace("]", "")
+			tempList.append(complex(num))
+		temp.append(tempList)
+	temp = np.array(temp, complexType)
+	if complexType == complex: temp = Complex32(temp)
+	csvfile.close()
+	return temp
+
+#### Load offline matrices ####
+with open('data/offline_matrices.csv', newline='') as csvfile:
+	test = csv.reader(csvfile, delimiter=';')
+	targetVar = "0"
+	for line in test:
+		if(line == []):
+			continue
+		try:
+			tempFloat = []
+			for num in line:
+				tempFloat.append(float(num))
+			if(targetVar == 'Af'):
+				Af.append(tempFloat)
+			elif(targetVar == 'Ab'):
+				Ab.append(tempFloat)
+			elif(targetVar == 'Bf'):
+				Bf.append(tempFloat)
+			elif(targetVar == 'Bb'):
+				Bb.append(tempFloat)
+			elif(targetVar == 'W'):
+				W.append(tempFloat)
+		except:
+			#Save which variable we are reading
+			targetVar = line[0]
+	#Convert lists to arrays
+	Af = np.array(Af)
+	Ab = np.array(Ab)
+	Bf = np.array(Bf)
+	Bb = np.array(Bb)
+	W = np.array(W)
+	csvfile.close()
+
+#### Load control signals ####
+with open('data/clean_signals.csv', newline='') as csvfile:
+	r = csv.reader(csvfile, delimiter=',')
+	for line in r:
+		tempList = []
+		for num in line:
+			num = num.replace('[', '')
+			num = num.replace(']', '')
+			tempList.append(int(num))
+		S.append(tempList)
+	S = np.array(S)
+	csvfile.close()
+
+#### Load parallel coefficients ####
+Fb = ReadFile('data/Fb.csv')
+Ff = ReadFile('data/Ff.csv')
+Lb = ReadFile('data/Lb.csv')
+Lf = ReadFile('data/Lf.csv')
+Wf = ReadFile('data/Wf.csv')
+Wb = ReadFile('data/Wb.csv')
+
+# The offline batch estimator algorithm we measure against
+def GoldenBatch():
+	M = np.zeros((N, signalLength + 1))
+	#Compute forward recursion
+	for k1 in range(1, signalLength):
+		M[:, k1] = np.dot(Af, M[:, k1 - 1]) + np.dot(Bf, S[:, k1 - 1])
+	#Compute backward recursion
+	for k2 in range(1, signalLength):
+		kk2 = signalLength - k2
+		tempM = np.dot(Ab, M[:, kk2]) + np.dot(Bb, S[:, kk2 - 1])
+		u_gold[kk2 - 1] = np.dot(W.T, (tempM - M[:, kk2 - 1]))
+		M[:, kk2 - 1] = tempM
+
+# Offline Parallel batch estimator
+def GoldenParallel():
+	Mf = np.zeros((N, signalLength), complex)
+	Mb = np.zeros((N, signalLength), complex)
+	for i in range(1, signalLength):
+		for m in range(0, N):
+			Mf[m, i] = Lf[m]*Mf[m, i-1] + Ff[m,0]*S[0, i-1] + Ff[m,1]*S[1, i-1] + Ff[m,2]*S[2, i-1]
+			Mb[m, signalLength-i-1] = Lb[m]*Mb[m, signalLength-i] + Fb[m,0]*S[0, signalLength-i-1] + Fb[m,1]*S[1, signalLength-i-1] + Fb[m,2]*S[2, signalLength-i-1]
+	for i in range(0, signalLength):
+		tempSum = 0
+		for m in range(0, N):
+			tempSum = tempSum + (Wf[m]*Mf[m, i]).real + (Wb[m]*Mb[m, i]).real
+		u_gold[i] = tempSum
+
+# Parallel online implementation 2
+# Baking all factors into LUTs so backward recursion is just summing
+def TestParallel2(Recursion):
+	Mf = np.zeros(N, complexType)
+	Lbw = np.zeros((N, Recursion), complexType)
+	# Pre-calculate constants
+	for i in range(0, N):
+		for j in range(0, Recursion):
+			Lbw[i][j] = (Wb[i] * Lb[i]**j)
+
+	#k is the current clock cycle
+	for k in range(0, signalLength):
+		for i in range(0, N):
+			#Calculate backward recursion
+			Mb = floatType(0)
+			for a in range(0, Recursion):
+				try:
+					tempS = S[:, k+a+1]
+				except:
+					tempS = np.array([0,0,0])
+				Mb = Mb + (Lbw[i][a] * np.dot(Fb[i,:],tempS)).real
+			#Calculate forward recursion and product
+			Mf[i] = (Lf[i]*Mf[i] + np.dot(Ff[i,:],S[:, k]))
+			if complexType == complex: Mf = Complex32(Mf)
+			u_par[k] = u_par[k] + Mb + (Wf[i] * Mf[i]).real
+
+# Online batch architecture
+def TestBatch(SampleSize):
+	Reg_Loading = np.zeros((N,SampleSize), floatType)
+	Reg_Lookahead = np.zeros((N,SampleSize), floatType)
+	M_LH = np.zeros(N, complexType)
+	Reg_PreComp = np.zeros((N,SampleSize), floatType)
+	Reg_Compute = np.zeros((N,SampleSize), floatType)
+	Mb = np.zeros(N, complexType)
+	Mf = np.zeros(N, complexType)
+	Reg_PartResultB = np.zeros((N,SampleSize), floatType)
+	Reg_PartResultF = np.zeros((N,SampleSize), floatType)
+	Reg_ResultB = np.zeros((N,SampleSize), floatType)
+	Reg_ResultF = np.zeros((N,SampleSize), floatType)
+	M_DF = np.zeros(N, floatType)
+	M_DB = np.zeros(N, floatType)
+	# Batch cycle
+	for k in range(0, signalLength + 4*SampleSize, SampleSize):
+		# Clock cycle i, everything in loop happens in parallel
+		for i in range(0, SampleSize):
+			j = SampleSize - i - 1
+			# Sample loading
+			try:
+				Reg_Loading[:,i] = S[:,k+i]
+			except:
+				Reg_Loading[:,i] = np.array([0, 0, 0])
+			# Calculation
+			for n in range(0, N):
+				# Lookahead stage
+				M_LH[n] = (Lb[n] * M_LH[n] + np.dot(Fb[n,:], Reg_Lookahead[:,j]))
+				if complexType == complex: M_LH = Complex32(M_LH) 
+				# Forward recursion
+				Mf[n] = (Lf[n] * Mf[n] + np.dot(Ff[n,:], Reg_Compute[:,i]))
+				if complexType == complex: Mf = Complex32(Mf)
+				Reg_PartResultF[n,i] = M_DF[n]
+				M_DF[n] = (Wf[n] * Mf[n]).real
+				# Backward Recursion
+				Mb[n] = (Lb[n] * Mb[n] + np.dot(Fb[n,:], Reg_Compute[:,j]))
+				if complexType == complex: Mb = Complex32(Mb)
+				Reg_PartResultB[n,j] = (Wb[n] * Mb[n]).real
+				# Output stage
+				k_delayed = k - 4*SampleSize
+				if ((k_delayed >= 0) and (k_delayed+i < signalLength)):
+					u_bat[k_delayed+i] = u_bat[k_delayed+i] + Reg_ResultB[n, i] + Reg_ResultF[n, i]
+		# Propagate registers
+		Reg_ResultF = np.array(Reg_PartResultF)
+		Reg_ResultB = np.array(Reg_PartResultB)
+		Mb = np.array(M_LH)
+		M_LH = np.zeros(N, complexType)
+		Reg_Compute = np.array(Reg_PreComp)
+		Reg_PreComp = np.array(Reg_Lookahead)
+		Reg_Lookahead = np.array(Reg_Loading)
+
+# Experimental architecture with reversed sample order
+# Becomes unstable when a rounding error occurs
+# Which is very common for float numbers
+def TestReverse(testLength):
+	M1 = np.complex64(0)			#Algo from batch code
+	M2 = np.complex128(0)			#Algo being tested
+	Lb_inv = (np.array([1/Lb[0], 1/Lb[1], 1/Lb[2]]))		#Inverted reverse lambda coeff
+	for i in range(0, testLength):
+		M1 = 0
+		for a in range(0,Lookahead):
+			if (i-a < 0):
+				break
+			M1 = Lb[0] * M1 + np.dot(Fb[0,:], S[:,i-a])
+		M2 = (Lb_inv[0] * M2) + (Lb[0]**(Lookahead-1) * np.dot(Fb[0,:], S[:,i]))
+		if (i-Lookahead >= 0):
+			M2 = M2 - (Lb_inv[0] * np.dot(Fb[0,:], S[:,i-Lookahead]))
+		print("i = " + str(i))
+		print(M1)
+		print(M2)
+		if ((abs(M1 - M2) > 0.0000001) and (i > Lookahead)):
+			break
+
+# Single pole IIR, fc = 100MHz
+def IIR_100(arr):
+	temp = arr
+	m = np.float16(0)
+	a0 = np.float16(0.712537755)
+	b1 = np.float16(0.28746224)
+	for i in range(0, len(arr)):
+		temp[i] = a0 * arr[i] + b1 * m
+		m = temp[i]
+	return temp
+
+def IIR_50(arr):
+	temp = arr
+	m = np.float16(0)
+	a0 = np.float16(0.46384494)
+	b1 = np.float16(0.53615505)
+	for i in range(0, len(arr)):
+		temp[i] = a0 * arr[i] + b1 * m
+		m = temp[i]
+	return temp
+
+def IIR_20(arr):
+	temp = arr
+	m = np.float16(0)
+	a0 = np.float16(0.22067939)
+	b1 = np.float16(0.77932061)
+	for i in range(0, len(arr)):
+		temp[i] = a0 * arr[i] + b1 * m
+		m = temp[i]
+	return temp
+
+# Biquad filter, fc = 10MHz
+def BIQ_10(arr):
+	temp = np.zeros(len(arr), np.float32)
+	m0 = np.float32(0)
+	m1 = np.float32(0)
+	m2 = np.float32(0)
+	a0 = np.float32(0.0035667865634507064)
+	a1 = np.float32(0.007133573126901413)
+	a2 = np.float32(0.0035667865634507064)
+	b1 = np.float32(1.824094670479931)
+	b2 = np.float32(-0.8383618167337338)
+	for i in range(0, len(arr)):
+		m0 = arr[i]
+		m0 = m0 + b1 * m1
+		m0 = m0 + b2 * m2
+		temp[i] = a0 * m0
+		temp[i] = temp[i] + a1 * m1
+		temp[i] = temp[i] + a2 * m2
+		m2 = m1
+		m1 = m0
+	return temp
+
+def BIQ_50(arr):
+	temp = np.zeros(len(arr), np.float32)
+	m0 = np.float32(0)
+	m1 = np.float32(0)
+	m2 = np.float32(0)
+	a0 = np.float32(0.06655775035418053)
+	a1 = np.float32(0.13311550070836106)
+	a2 = np.float32(0.06655775035418053)
+	b1 = np.float32(1.1494245244651997)
+	b2 = np.float32(-0.41565552588192206)
+	for i in range(0, len(arr)):
+		m0 = arr[i]
+		m0 = m0 + b1 * m1
+		m0 = m0 + b2 * m2
+		temp[i] = a0 * m0
+		temp[i] = temp[i] + a1 * m1
+		temp[i] = temp[i] + a2 * m2
+		m2 = m1
+		m1 = m0
+	return temp
+
+def BIQ_20(arr):
+	temp = np.zeros(len(arr), np.float32)
+	m0 = np.float32(0)
+	m1 = np.float32(0)
+	m2 = np.float32(0)
+	a0 = np.float32(0.013164365887905859)
+	a1 = np.float32(0.026328731775811718)
+	a2 = np.float32(0.013164365887905859)
+	b1 = np.float32(1.6502158334595687)
+	b2 = np.float32(-0.7028732970111922)
+	for i in range(0, len(arr)):
+		m0 = arr[i]
+		m0 = m0 + b1 * m1
+		m0 = m0 + b2 * m2
+		temp[i] = a0 * m0
+		temp[i] = temp[i] + a1 * m1
+		temp[i] = temp[i] + a2 * m2
+		m2 = m1
+		m1 = m0
+	return temp
+
+# Find the largest absolute and relative error
+def GetPeakError(arr):
+	e = 0
+	f = 0
+	for i in range(100, signalLength-100):
+		e = max(e, abs(arr[i] - u_gold[i]))
+		f = max(f, abs((arr[i]-u_gold[i])/u_gold[i]))
+	return e, f
+
+# Plot a section of the wave
+def PlotWave(arr, length, tit):
+	k = np.arange(0, length)
+	plt.title(tit)
+	plt.xlabel("time k")
+	plt.ylabel("result u")
+	plt.plot(k, arr[:length])
+
+# Plot the PSD and return SNR
+def PlotPSD(arr, tit, sig_leak=0):
+	T = 1.0 / f_clk
+	
+	arr_f, freq = plt.psd(arr[512:-3584], NFFT=signalLength-4096, Fs=f_clk)
+	plt.xscale('log')
+	plt.grid(True)
+
+	#Find signal position
+	sigpos = max(range(len(arr_f)), key=lambda i: abs(arr_f[i]))
+
+	#Normalize
+	#arr_f = arr_f/arr_f[sigpos]
+
+	#Calculate signal power
+	Ps = 0
+	for i in range(sigpos-sig_leak, sigpos+sig_leak+1):
+		Ps = Ps + arr_f[i]
+		arr_f[i] = 0
+	
+	#Calculate noise power
+	Pn = sum(arr_f)
+
+	SNR = 10*np.log10(Ps/Pn)
+
+	return SNR
+
+# Run online batch test and generate figure
+def RunBatchTest(buff):
+	global u_bat
+	u_bat = np.zeros(signalLength, floatType)
+	TestBatch(buff)
+	#u_bat = BIQ_10(u_bat)
+	plt.figure(figsize=(10, 8))
+	plt.subplot(2,1,1)
+	PlotWave(u_bat, 1536, "Waveform with buffer size " + str(buff))
+
+	plt.subplot(2,1,2)
+	SNR = PlotPSD(u_bat, "FFT for buffer size " + str(buff), 1)
+	plt.figtext(0.7, 0.4, "SNR = " + ('%.2f' % SNR) + "dB")
+	plt.savefig(("plots_" + typeLabel + "/BatchPlot_" + str(buff)))
+	plt.close()
+	return SNR
+
+# Run online parallel test and generate figure
+def RunParaTest(buff):
+	global u_par
+	u_par = np.zeros(signalLength, floatType)
+	TestParallel2(buff)
+	#u_par = BIQ_10(u_par)
+	plt.figure(figsize=(10, 8))
+	plt.subplot(2,1,1)
+	PlotWave(u_par, 1536, "Waveform with " + str(buff) + " recursion stages")
+
+	plt.subplot(2,1,2)
+	SNR = PlotPSD(u_par, "FFT for " + str(buff) + " recursion stages", 1)
+	plt.figtext(0.7, 0.4, "SNR = " + ('%.2f' % SNR) + "dB")
+	plt.savefig(("plots_" + typeLabel + "/ParPlot_" + str(buff)))
+	plt.close()
+	return SNR
+
+def RunGolden(mode):
+	global u_gold
+	u_gold = np.zeros(signalLength)
+
+	# Run test
+	if mode == 1:
+		GoldenBatch()
+		# Plot figure
+		plt.figure(figsize=(10,8))
+		plt.subplot(2,1,1)
+		PlotWave(u_gold, 1536, "Regular offline algorithm")
+		plt.subplot(2,1,2)
+		SNR = PlotPSD(u_gold, "FFT", 1)
+		plt.figtext(0.7, 0.4, "SNR = " + ('%.2f' % SNR) + "dB")
+		plt.savefig("plots_64bit/RegularFFT")
+	else:
+		GoldenParallel()
+		plt.figure(figsize=(10,8))
+		plt.subplot(2,1,1)
+		PlotWave(u_gold, 1536, "Offline parallel algorithm")
+		plt.subplot(2,1,2)
+		SNR = PlotPSD(u_gold, "FFT", 1)
+		plt.figtext(0.7, 0.4, "SNR = " + ('%.2f' % SNR) + "dB")
+		plt.savefig("plots_" + typeLabel + "/ParallelFFT")
+
+def RunSNRBatch(top, step):
+	SNR_Batch = []
+	# Simulate
+	x = np.arange(16, top, step)
+	for langth in x:
+		print("Testing batch with parameter " + str(langth) + "...")
+		SNR_Batch.append(RunBatchTest(langth))
+	# Display every 4th tick
+	xdisp = []
+	for i in range(0, x.size):
+		if (i % 4 == 0):
+			xdisp.append(str(x[i]))
+		else:
+			xdisp.append("")
+	# Plot figure
+	peak = np.amax(SNR_Batch)
+	where = np.where(SNR_Batch == peak)
+	plt.figure(figsize=(10,8))
+	plt.plot(x, SNR_Batch)
+	plt.title("SNR for batch architecture - " + typeLabel)
+	plt.xlabel("Buffer size")
+	plt.ylabel("SNR - dB")
+	plt.minorticks_off()
+	plt.xticks(x, xdisp, rotation=45)
+	plt.figtext(0.6, 0.85, "Peak is " + ('%.2f' % peak) + "dB with buffer size = " + str(int(x[where])))
+	plt.grid(True)
+	plt.savefig("plots_" + typeLabel + "/SNR_Batch_" + typeLabel)
+
+def RunSNRParallell(top, step):
+	SNR_Para = []
+	# Run simulations
+	x = np.arange(16, top, step)
+	for langth in x:
+		print("Testing parallel with parameter " + str(langth) + "...")
+		SNR_Para.append(RunParaTest(langth))
+	# Display every 4th tick
+	xdisp = []
+	for i in range(0, x.size):
+		if (i % 4 == 0):
+			xdisp.append(str(x[i]))
+		else:
+			xdisp.append("")
+	# Plot figure
+	peak = np.amax(SNR_Para)
+	where = np.where(SNR_Para == peak)
+	plt.figure(figsize=(10,8))
+	plt.plot(x, SNR_Para)
+	plt.title("SNR for parallel achitecture - " + typeLabel)
+	plt.xlabel("Recursion stages")
+	plt.ylabel("SNR - dB")
+	plt.minorticks_off()
+	plt.xticks(x, xdisp, rotation=45)
+	plt.figtext(0.58, 0.85, "Peak is " + ('%.2f' % peak) + "dB with recursion stages = " + str(int(x[where])))
+	plt.grid(True)
+	plt.savefig("plots_" + typeLabel + "/SNR_Parallel_" + typeLabel)
+
+
+RunGolden(0)
+RunGolden(1)
+
+RunBatchTest(32768)
+RunSNRBatch(513, 16)
+RunSNRParallell(513, 16)
+
