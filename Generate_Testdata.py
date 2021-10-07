92b211d73210e3070aaadf56692f54606f4cb1ae
GuavTek <36814224+GuavTek@users.noreply.github.com>
2021-10-07 18:40:02 +0200

    Generate FIR coefficients

--- Generate_Testdata.py
+++ Generate_Testdata.py
@@ -4,7 +4,8 @@ import numpy as np
 
 N = 3   # Analog states
 M = N   # Digital states
-samples_num = 32768     # Length of generated test data
+samples_num = 100 #32768     # Length of generated test data
+FIR_size = 256
 
 f_clk = 240e6   # ADC sampling frequency
 OSR = 12        # Oversampling ratio
@@ -39,6 +40,7 @@ print(digital_control)
 digital_estimator = cbadc.digital_estimator.DigitalEstimator(analog_system, digital_control, eta2, samples_num)
 print(digital_estimator)
 
+# Write to csv files
 adc.WriteCSVFile("data/Ab", digital_estimator.Ab)
 adc.WriteCSVFile("data/Af", digital_estimator.Af)
 adc.WriteCSVFile("data/Bb", digital_estimator.Bb)
@@ -68,3 +70,50 @@ for s in simulator:
     x += 1
 
 adc.WriteCSVFile("data/clean_signals2", tVectors)
+
+# Prepare FIR parameters
+omega_3dB = 2 * np.pi /(T * OSR)
+G_at_omega = np.linalg.norm(analog_system.transfer_function_matrix(np.array([omega_3dB/2])))
+eta2 = G_at_omega**2
+L1 = FIR_size
+L2 = FIR_size
+
+# Instantiate FIR filter without Downsampling
+FIR_estimator_ref = cbadc.digital_estimator.FIRFilter(analog_system, digital_control, eta2, L1, L2)
+print(FIR_estimator_ref)
+
+# Write Coefficients to files
+h1 = FIR_estimator_ref.h[0][0:FIR_size-1]
+h2 = FIR_estimator_ref.h[0][FIR_size:-1]
+print("h1 = ")
+print(h1)
+print("h2 = ")
+print(h2)
+adc.WriteCSVFile("data/FIR1_h1", h1)
+adc.WriteCSVFile("data/FIR1_h2", h2)
+
+# Create anti-alias filter
+wp = omega_3dB / 2.0
+ws = omega_3dB
+gpass = 0.1
+gstop = 80
+
+filter = cbadc.analog_system.IIRDesign(wp, ws, gpass, gstop, ftype="ellip")
+
+# Create new analog system with filter
+analog_system_new = cbadc.analog_system.chain([filter, analog_system])
+print(analog_system_new)
+
+# Instantiate FIR filter with downsampling
+FIR_estimator_ds = cbadc.digital_estimator.FIRFilter(analog_system_new, digital_control, eta2, L1, L2, downsample=OSR)
+print(FIR_estimator_ds)
+
+# Write coefficients to file
+h1 = FIR_estimator_ds.h[0][0:FIR_size-1]
+h2 = FIR_estimator_ds.h[0][FIR_size:-1]
+print("h1 = ")
+print(h1)
+print("h2 = ")
+print(h2)
+adc.WriteCSVFile("data/FIR" + str(OSR) + "_h1", h1)
+adc.WriteCSVFile("data/FIR" + str(OSR) + "_h2", h2)
