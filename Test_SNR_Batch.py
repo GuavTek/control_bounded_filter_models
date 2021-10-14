745407494cb62b0174d25b5cf877805a2facb4de
GuavTek <36814224+GuavTek@users.noreply.github.com>
2021-10-14 23:44:29 +0200

    Update test scripts

--- Test_SNR_Batch.py
+++ Test_SNR_Batch.py
@@ -3,27 +3,30 @@ from HardCB import HardCB
 
 OverRate = 1
 top = 240
+bottom = 80
 step = 20
 
-adc = HardCB()
-adc.f_clk = 240e6
-adc.SetSystemOrder(3)
+adc = HardCB(4)
+adc.f_clk = 250e6
 adc.SetFloatBitWidth(32)
 adc.SetPlotDirectory('test_plot')
 adc.ReadOfflineFiles('data')
 adc.ReadIIRCoefficients('data')
-adc.ReadStimuliFile('data/clean_signals2')
+adc.ReadStimuliFile('data/clean_signals')
 
-#golden = adc.GoldenBatch()
-#adc.PlotFigure(golden, int(round(1536 / OverRate)), "Golden Batch architecture", 'GoldBatch')
+golden = adc.GoldenBatch()
+adc.PlotFigure(golden[2000:-2000], int(round(1000 / OverRate)), "Golden Batch architecture", 'GoldBatch')
+
+golden = adc.GoldenParallel()
+adc.PlotFigure(golden[2000:-2000], int(round(1000 / OverRate)), "Golden Parallel architecture", 'GoldParallelBatch')
 
 
 SNR_Batch = []
 # Simulate
-x = np.arange(step, top+1, step)
+x = np.arange(bottom, top+1, step)
 for langth in x:
 	print("Testing batch with parameter " + str(langth) + "...")
 	results = adc.BatchIIR(langth, OverRate)
-	SNR_Batch.append(adc.PlotFigure(results[1920:-1920], int(round(960 / OverRate)), "Batch architecture - batch size = " + str(langth), "Batch_" + str(langth)))
+	SNR_Batch.append(adc.PlotFigure(results[2000:-2000], int(round(1000 / OverRate)), "Batch architecture - batch size = " + str(langth), "Batch_" + str(langth)))
 	
 adc.PlotSNR(x, SNR_Batch, "SNR for Batch architecture - 32bit", "Batch Size", "SNRBatch_32bit")
--- Test_SNR_FIR.py
+++ Test_SNR_FIR.py
@@ -3,28 +3,28 @@ from HardCB import HardCB
 
 OverRate = 1
 top = 240
+bottom = 80
 step = 20
 
-adc = HardCB()
-adc.f_clk = 240e6
-adc.SetSystemOrder(3)
+adc = HardCB(4)
+adc.f_clk = 250e6
 adc.SetFloatBitWidth(32)
 adc.SetPlotDirectory('test_plot')
 adc.ReadOfflineFiles('data')
 adc.ReadFIRCoefficients('data', OverRate)
-adc.ReadStimuliFile('data/clean_signals2')
+adc.ReadStimuliFile('data/clean_signals')
 
 golden = adc.GoldenBatch()
-adc.PlotFigure(golden[1920:-1920], int(round(1536 / OverRate)), "Golden Batch architecture", 'GoldBatch')
+adc.PlotFigure(golden[2000:-2000], int(round(1000 / OverRate)), "Golden Batch architecture", 'GoldBatch')
 
 
 SNR_FIR = []
 adc.osr = OverRate
 # Simulate
-x = np.arange(step, top+1, step)
+x = np.arange(bottom, top+1, step)
 for langth in x:
 	print("Testing FIR with length " + str(langth) + "...")
 	results = adc.FIR(langth, OverRate)
-	SNR_FIR.append(adc.PlotFigure(results[int(1920/OverRate):int(-1920/OverRate)], int(round(960 / OverRate)), "FIR architecture - length = " + str(langth), "FIR_" + str(langth)))
+	SNR_FIR.append(adc.PlotFigure(results[int(2000/OverRate):int(-2000/OverRate)], int(round(1000 / OverRate)), "FIR architecture - length = " + str(langth), "FIR_" + str(langth)))
 
 adc.PlotSNR(x, SNR_FIR, "SNR for FIR architecture - 32bit", "FIR length", "SNRFIR_32bit")
